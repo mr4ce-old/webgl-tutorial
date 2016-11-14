0198e86e04a2c6686f29ea0fbf74ddc993e3c170
mr4ce <edward.ghore@royal-arts.co.uk>
2016-11-14 15:43:43 +0200

    canvas.js edited

--- canvas.js
+++ canvas.js
@@ -1,1 +1,8 @@
 var canvas = document.getElementById('my-canvas');
+var context = canvas.getContext('2d');
+
+// getContext() used to obtain the rendering context and its drawing functions. This method takes one parameter, the type of context 2d
+
+context.font = '20pt Calibri';
+context.fillStyle = 'green';
+context.fillText('Welcome to tutorialspoint', 70, 70);
