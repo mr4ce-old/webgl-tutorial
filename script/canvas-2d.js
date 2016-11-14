ab911c6e5edae3d3b2cfe45ecab62866a48c01d0
mr4ce <edward.ghore@royal-arts.co.uk>
2016-11-14 15:50:41 +0200

    canvas.js renamed to canvas-2d.js

--- script/canvas-2d.js
+++ script/canvas-2d.js
@@ -1,7 +1,10 @@
 var canvas = document.getElementById('my-canvas');
 var context = canvas.getContext('2d');
 
-// getContext() used to obtain the rendering context and its drawing functions. This method takes one parameter, the type of context 2d
+/*
+ getContext() used to obtain the rendering context and its drawing functions. This method takes one parameter,
+ the type of context 2d
+ */
 
 context.font = '20pt Calibri';
 context.fillStyle = 'green';
